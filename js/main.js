a2239874e850fb36b219e797adbbde604b9e37ca
psvel-332 <sakthivelpk2002@gmail.com>
2023-03-10 23:42:35 +0530

    niwork

--- js/main.js
+++ js/main.js
@@ -536,3 +536,15 @@ $(document).ready(function () {
 	});
   })
   (jQuery);
+
+  $(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items:3,
+  //      autoplay:false,
+		margin:30,
+		loop:true,
+		dots:true
+  //      nav:true,
+  //      navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
+	});
+  });
